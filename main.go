865ed3596927eadebf0e82b306dede8356e5c87c
Lucille Cénac <lucille.cenac@ynov.com>
2023-06-05 16:18:37 +0200

    add: -dockerfile -docker compose -.env.example -database

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"strconv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
-	database, _ := sql.Open("sqlite3", "./test.db")
-	modif, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, NAME VARCHAR)")
-	modif.Exec()
-	modif, _ = database.Prepare("INSERT INTO users(name) VALUES (?)")
-	modif.Exec("test")
-	rows, _ := database.Query("SELECT id, name From users")
-	var id int
-	var name string
-	for rows.Next() {
-		rows.Scan(&id, &name)
-		fmt.Println(strconv.Itoa(id) + ": " + name)
+	db, err := gorm.Open(postgres.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
 	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
+
+	// Update - update product's price to 200
+	db.Model(&product).Update("Price", 200)
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - delete product
+	db.Delete(&product, 1)
 }
