a55a03fae841ba3ad330bb3beb03e6cea929becd
Brun <sasha.brun@ynov.com>
2023-06-06 16:50:27 +0200

    Modif name and add Like Comment

---
+++ backend/database/likeComment/likeComment_services.go
@@ -0,0 +1,48 @@
+package likeComment
+
+import (
+	"exploreur/backend/database"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type LikeComment struct {
+	gorm.Model
+	isLike    bool
+	isDislike bool
+}
+
+var likeComment = &LikeComment{}
+
+func AddLikeComment(isLike bool, isDislike bool) {
+	db, err := gorm.Open(postgres.Open(database.GetEnv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.Create(&LikeComment{isLike: isLike, isDislike: isDislike})
+}
+
+func DeleteLikeComment(id int) {
+	db, err := gorm.Open(postgres.Open(database.GetEnv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.Delete(&LikeComment{}, id)
+}
+
+func UpdateLikeCommentIsLike(isLike bool, id int) {
+	db, err := gorm.Open(postgres.Open(database.GetEnv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.Model(&LikeComment{}).Where("id = ?", id).Update("isLike", isLike)
+}
+
+func UpdateLikeCommentIsDislike(isDislike bool, id int) {
+	db, err := gorm.Open(postgres.Open(database.GetEnv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.Model(&LikeComment{}).Where("id = ?", id).Update("isDislike", isDislike)
+}
