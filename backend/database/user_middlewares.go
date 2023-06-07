ef77e79566592e94671ca47a40b6a16761b4edd7
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2023-06-07 15:03:27 +0200

    add: - middlewares to check if not duplicate email and nickname

--- backend/database/user_middlewares.go
+++ backend/database/user_middlewares.go
@@ -1,3 +1,25 @@
 package database
 
 // TODO register
+
+func IfNicknameExist(nickname string) bool {
+	id, err := GetIDByNickname(nickname)
+	if err != nil {
+		panic(err)
+	}
+	if id == -1 {
+		return false
+	}
+	return true
+}
+
+func IfEmailExist(email string) bool {
+	id, err := GetIDByEmail(email)
+	if err != nil {
+		panic(err)
+	}
+	if id == -1 {
+		return false
+	}
+	return true
+}
