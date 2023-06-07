466a6f2f197772433ee14c02cde1c89ad1b0be6e
Lucille Cénac <lucille.cenac@ynov.com>
2023-06-07 14:56:40 +0200

    add : - check email

--- backend/database/user_middlewares.go
+++ backend/database/user_middlewares.go
@@ -1,3 +1,12 @@
 package database
 
+import (
+	"regexp"
+)
+
 // TODO register
+
+func CheckEmail(str string) bool {
+	re := regexp.MustCompile(`^[0-9a-z!#$%&'*+–/=?^_.{|}~]{1,64}@[a-z]{1,63}\.[a-z]{1,3}$`)
+	return re.MatchString(str)
+}
