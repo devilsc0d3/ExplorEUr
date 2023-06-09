ba089e987cb12d1b229ba663d63fcf2e36329739
devilsc0d3 <faureleo736@gmail.com>
2023-06-09 16:16:42 +0200

    update of category page

--- backend/server/server.go
+++ backend/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -17,11 +18,15 @@ const port = "8080"
 
 func Server() {
 	router()
+	fmt.Println("Listening on http://localhost:" + port)
 	fmt.Println("Listening on https://localhost:" + port)
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		return
-	}
+	//err := http.ListenAndServe(":"+port, nil)
+	//if err != nil {
+	//	return
+	//}
 
 	//log.Fatal(http.ListenAndServeTLS(":"+port, "./localhost.crt", "localhost.key", nil))
+
+	err := http.ListenAndServeTLS(":8080", "localhost.crt", "localhost.key", nil)
+	log.Fatal(err)
 }
