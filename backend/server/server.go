b81f84a0d681474689b18e3fa8ef06f64d2d3ab1
Lucille Cénac <lucille.cenac@ynov.com>
2023-06-12 16:56:08 +0200

    add: chat page

--- backend/server/server.go
+++ backend/server/server.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 )
+
+var data = []string{"Place", "Tools", "Information", "+"}
 
 func router() {
 	fs := http.FileServer(http.Dir("./front/static"))
@@ -13,17 +15,47 @@ func router() {
 	http.HandleFunc("/category", category)
 	http.HandleFunc("/login", Login)
 	http.HandleFunc("/registration", Registration)
+
+	for i := 0; i < len(data); i++ {
+		http.HandleFunc("/"+data[i], Chat)
+	}
+	Reset()
 }
 
 const port = "8080"
 
 func Server() {
 	router()
-	fmt.Println("Listening on https://localhost:" + port)
-	//err := http.ListenAndServe(":"+port, nil)
-	//if err != nil {
-	//	return
-	//}
+	fmt.Println("Listening on http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		return
+	}
 
-	log.Fatal(http.ListenAndServeTLS(":"+port, "./localhost.crt", "localhost.key", nil))
+	//log.Fatal(http.ListenAndServeTLS(":"+port, "./localhost.crt", "localhost.key", nil))
+}
+
+func Reset() {
+
+	client := &http.Client{}
+
+	// create a new DELETE request
+	req, err := http.NewRequest("DELETE", "http://localhost:8080/registration", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(body)
 }
--- backend/server/server_handlers.go
+++ backend/server/server_handlers.go
@@ -6,7 +6,7 @@ import (
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
-	page, _ := template.ParseFiles("./../front/template/home.html")
+	page, _ := template.ParseFiles("./front/template/home.html")
 	err := page.ExecuteTemplate(w, "home.html", nil)
 	if err != nil {
 		return
@@ -14,9 +14,9 @@ func home(w http.ResponseWriter, _ *http.Request) {
 }
 
 func category(w http.ResponseWriter, _ *http.Request) {
-	page, _ := template.ParseFiles("./../front/template/category.html")
+	page, _ := template.ParseFiles("./front/template/category.html")
 
-	dataTest := []string{"plAce", "Tools", "informatiOn", "+"}
+	dataTest := []string{"Place", "Tools", "Information", "+"}
 
 	err := page.ExecuteTemplate(w, "category.html", dataTest)
 	if err != nil {
@@ -35,6 +35,14 @@ func Login(w http.ResponseWriter, _ *http.Request) {
 func Registration(w http.ResponseWriter, _ *http.Request) {
 	page, _ := template.ParseFiles("./front/template/registration.html")
 	err := page.ExecuteTemplate(w, "registration.html", nil)
+	if err != nil {
+		return
+	}
+}
+
+func Chat(w http.ResponseWriter, _ *http.Request) {
+	page, _ := template.ParseFiles("./front/template/chat.html")
+	err := page.ExecuteTemplate(w, "chat.html", nil)
 	if err != nil {
 		return
 	}
