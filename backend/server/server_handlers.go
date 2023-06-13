8b31e766edeaff9cbf819ad8a40797dd0042c810
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2023-06-13 16:09:09 +0200

    add: - registration pop-ups

--- backend/server/server_handlers.go
+++ backend/server/server_handlers.go
@@ -12,6 +12,7 @@ func Router() {
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/category", CategoryHandler)
 	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/register", Register)
 	http.HandleFunc("/registration", RegistrationHandler)
 }
 
@@ -40,14 +41,31 @@ func LoginHandler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func RegistrationHandler(w http.ResponseWriter, r *http.Request) {
-	page, _ := template.ParseFiles("./front/template/registration.html")
+func Register(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("nickname") != "" && r.FormValue("email") != "" && r.FormValue("password") != "" && r.FormValue("confirmation") == r.FormValue("password") {
-		user := registerDB.AddUserController(r.FormValue("nickname"), r.FormValue("email"), r.FormValue("password"))
-		if user == "" {
-			http.Redirect(w, r, "/", 303)
+		userError := registerDB.AddUserController(r.FormValue("nickname"), r.FormValue("email"), r.FormValue("password"))
+		if userError == "" {
+			http.Redirect(w, r, "/login", 303)
+		} else if userError != "" {
+			if registerDB.IfNicknameExist(r.FormValue("nickname")) {
+				http.Redirect(w, r, "/registration?error=nickname-already-used", 303)
+			}
+			if registerDB.IfEmailExist(r.FormValue("email")) {
+				http.Redirect(w, r, "/registration?error=email-already-used", 303)
+			}
+			if !registerDB.CheckEmail(r.FormValue("email")) {
+				http.Redirect(w, r, "/registration?error=email-not-valid", 303)
+			}
+			if !registerDB.CheckPassword(r.FormValue("password")) {
+				http.Redirect(w, r, "/registration?error=password-not-valid", 303)
+			}
 		}
 	}
+	http.Redirect(w, r, "/registration", 303)
+}
+
+func RegistrationHandler(w http.ResponseWriter, r *http.Request) {
+	page, _ := template.ParseFiles("./front/template/registration.html")
 	err := page.ExecuteTemplate(w, "registration.html", nil)
 	if err != nil {
 		return
